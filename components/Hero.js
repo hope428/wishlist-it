5f92e2bf457fda0361baf319392212c23d0724fe
Leon Forsythe <lvforsythe95@gmail.com>
2023-03-09 17:09:52 -0500

    added styling to navbar and hero image

---
+++ components/Hero.js
@@ -0,0 +1,11 @@
+import classes from "./Hero.module.css";
+
+function Hero() {
+  return (
+    <div className={classes.background}>
+      <div className={classes.layer}></div>
+    </div>
+  );
+}
+
+export default Hero;
--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,9 +1,12 @@
 import classes from './Navbar.module.css'
+import {Courgette} from 'next/font/google'
+
+const courgette = Courgette({weight: '400', subsets: ['latin']})
 
 function Navbar(){
     return (
         <nav className={classes.navbar}>
-            <h1 className={classes.logo}>Wishlist It</h1>
+            <h1 className={`${classes.logo} ${courgette.className}`}>Wishlist It</h1>
         </nav>
     )
 }
